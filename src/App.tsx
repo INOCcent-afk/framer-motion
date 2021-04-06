3901a76579e8eb7f7eb3ca13fda3985f9c802b61
Dave Inoc <michael828inoc@gmail.com>
2021-04-07 00:15:12 +0800

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
-function App() {
+const Page1 = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div
+      initial={{ scaleY: 0 }}
+      animate={{ scaleY: 1 }}
+      exit={{ scaleY: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div style={{ ...styles.page, ...styles.page1 }}>
+        <p style={styles.copy}>This is page 1</p>
+        <Link style={{ ...styles.copy, ...styles.link }} to="/page2">
+          Go to Page 2
+        </Link>
+      </div>
+    </motion.div>
   );
-}
+};
+
+const Page2 = () => {
+  return (
+    <motion.div
+      initial={{ scaleY: 0 }}
+      animate={{ scaleY: 1 }}
+      exit={{ scaleY: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div style={{ ...styles.page, ...styles.page2 }}>
+        <p style={styles.copy}>This is page 2</p>
+        <Link style={{ ...styles.copy, ...styles.link }} to="/">
+          Go to Page 1
+        </Link>
+      </div>
+    </motion.div>
+  );
+};
+
+const App = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <Switch location={location} key={location.pathname}>
+        <Route path="/" exact>
+          <Page1 />
+        </Route>
+        <Route path="/page2" exact>
+          <Page2 />
+        </Route>
+      </Switch>
+    </AnimatePresence>
+  );
+};
+
+const styles: Record<string, React.CSSProperties> = {
+  page: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100vw",
+    height: "100vh",
+    flexGrow: 1,
+  },
+  page1: {
+    backgroundColor: "#fe4a49",
+  },
+  page2: {
+    backgroundColor: "#2ab7ca",
+  },
+  copy: {
+    color: "white",
+    fontFamily: "Helvetica Neue, sans-serif",
+    fontWeight: "bold",
+    fontSize: 32,
+  },
+  link: {
+    color: "white",
+    display: "block",
+    marginTop: 10,
+  },
+};
 
 export default App;
